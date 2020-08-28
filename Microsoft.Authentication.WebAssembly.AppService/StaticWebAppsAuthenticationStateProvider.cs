6b17e7790dcc44ee329173d7a2bcdf203ba56f91
Aaron Powell <me@aaron-powell.com>
2020-08-28 15:32:22 +1000

    extracting the parse logic to a new function

--- Microsoft.Authentication.WebAssembly.AppService/StaticWebAppsAuthenticationStateProvider.cs
+++ Microsoft.Authentication.WebAssembly.AppService/StaticWebAppsAuthenticationStateProvider.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Authentication.WebAssembly.AppService.Models;
+using System.Text.Json;
 
 namespace Microsoft.Authentication.WebAssembly.AppService
 {
@@ -26,27 +27,35 @@ namespace Microsoft.Authentication.WebAssembly.AppService
         {
             try
             {
-                var authDataUrl = config.GetValue<string>("StaticWebAppsAuthentication:AuthenticationDataUrl", "/.auth/me");
-                var data = await http.GetFromJsonAsync<AuthenticationData>(authDataUrl);
+                var authDataUrl = config.GetValue("StaticWebAppsAuthentication:AuthenticationDataUrl", "/.auth/me");
+                var json = await http.GetStringAsync(authDataUrl);
 
-                var principal = data.ClientPrincipal;
-                principal.UserRoles = principal.UserRoles.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
-
-                if (!principal.UserRoles.Any())
-                {
-                    return new AuthenticationState(new ClaimsPrincipal());
-                }
-
-                var identity = new ClaimsIdentity(principal.IdentityProvider);
-                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
-                identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
-                identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
-                return new AuthenticationState(new ClaimsPrincipal(identity));
+                var user = ParseClaims(json);
+                return new AuthenticationState(user);
             }
             catch
             {
                 return new AuthenticationState(new ClaimsPrincipal());
             }
+        }
+
+        public static ClaimsPrincipal ParseClaims(string json)
+        {
+            var data = JsonSerializer.Deserialize<AuthenticationData>(json);
+
+            var principal = data.ClientPrincipal;
+            principal.UserRoles = principal.UserRoles.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+            if (!principal.UserRoles.Any())
+            {
+                return new ClaimsPrincipal();
+            }
+
+            var identity = new ClaimsIdentity(principal.IdentityProvider);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+            return new ClaimsPrincipal(identity);
         }
     }
 }
