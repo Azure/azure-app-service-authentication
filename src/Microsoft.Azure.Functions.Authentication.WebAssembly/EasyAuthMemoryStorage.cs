7ed59092d68547b459941ec974c3854fa1b0d67b
Mitali Karmarkar <mikarmar@microsoft.com>
2021-05-04 17:40:22 -0700

    rename

--- src/Microsoft.Azure.Functions.Authentication.WebAssembly/EasyAuthMemoryStorage.cs
+++ src/Microsoft.Azure.Functions.Authentication.WebAssembly/EasyAuthMemoryStorage.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Microsoft.Azure.AppService.Authentication.WebAssembly.Models;
+using Microsoft.Azure.Functions.Authentication.WebAssembly .Models;
 
-namespace Microsoft.Azure.AppService.Authentication.WebAssembly
+namespace Microsoft.Azure.Functions.Authentication.WebAssembly 
 {
     // A simple in-memory storage model for caching auth data
     class EasyAuthMemoryStorage
